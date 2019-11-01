c011d49228ad098c6f13eb02dee0db6331df59a7
Gaurav Sharma <gaurav.itachi@gmail.com>
2019-11-02 00:12:17 +0530

    test spring boot application

---
+++ src/test/java/com/gauravitachi/testapp/helloworld/HelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauravitachi.testapp.helloworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
